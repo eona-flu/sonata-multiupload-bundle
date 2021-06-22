0417824ca35ce8bf9a1f8781c85cf6891a94b35d
eona-flu <85988716+eona-flu@users.noreply.github.com>
2021-06-22 10:14:52 +0200

    Update MultiUploadController.php

--- src/Controller/MultiUploadController.php
+++ src/Controller/MultiUploadController.php
@@ -3,18 +3,21 @@
 namespace SilasJoisten\Sonata\MultiUploadBundle\Controller;
 
 use SilasJoisten\Sonata\MultiUploadBundle\Form\MultiUploadType;
+use Sonata\AdminBundle\Controller\CRUDController;
 use Sonata\Doctrine\Model\ManagerInterface;
 use Sonata\MediaBundle\Controller\MediaAdminController;
 use Sonata\MediaBundle\Model\MediaInterface;
 use Sonata\MediaBundle\Provider\MediaProviderInterface;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormRenderer;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @codeCoverageIgnore
  */
-class MultiUploadController extends MediaAdminController
+class MultiUploadController extends CRUDController
 {
     /**
      * @var ManagerInterface
@@ -26,7 +29,7 @@ class MultiUploadController extends MediaAdminController
         $this->mediaManager = $mediaManager;
     }
 
-    public function createAction(Request $request = null)
+    public function createAction(Request $request): Response
     {
         $this->admin->checkAccess('create');
 
@@ -42,6 +45,64 @@ class MultiUploadController extends MediaAdminController
         }
 
         return parent::createAction($request);
+    }
+
+    public function listAction(Request $request): Response
+    {
+        $this->admin->checkAccess('list');
+
+        if ($listMode = $request->get('_list_mode', 'mosaic')) {
+            $this->admin->setListMode($listMode);
+        }
+
+        $datagrid = $this->admin->getDatagrid();
+
+        $filters = $request->get('filter');
+
+        // set the default context
+        if (!$filters || !\array_key_exists('context', $filters)) {
+            $context = $this->admin->getPersistentParameter('context', $this->get('sonata.media.pool')->getDefaultContext());
+        } else {
+            $context = $filters['context']['value'];
+        }
+
+        $datagrid->setValue('context', null, $context);
+
+        $rootCategory = null;
+        if ($this->has('sonata.media.manager.category')) {
+            $rootCategory = $this->get('sonata.media.manager.category')->getRootCategory($context);
+        }
+
+        if (null !== $rootCategory && !$filters) {
+            $datagrid->setValue('category', null, $rootCategory->getId());
+        }
+        if ($this->has('sonata.media.manager.category') && $request->get('category')) {
+            $category = $this->get('sonata.media.manager.category')->findOneBy([
+                'id' => (int) $request->get('category'),
+                'context' => $context,
+            ]);
+
+            if (!empty($category)) {
+                $datagrid->setValue('category', null, $category->getId());
+            } else {
+                $datagrid->setValue('category', null, $rootCategory->getId());
+            }
+        }
+
+        $formView = $datagrid->getForm()->createView();
+
+        $twig = $this->get('twig');
+
+        // set the theme for the current Admin Form
+        $twig->getRuntime(FormRenderer::class)->setTheme($formView, $this->admin->getFilterTheme());
+
+        return $this->renderWithExtraParams($this->admin->getTemplateRegistry()->getTemplate('list'), [
+            'action' => 'list',
+            'form' => $formView,
+            'datagrid' => $datagrid,
+            'root_category' => $rootCategory,
+            'csrf_token' => $this->getCsrfToken('sonata.batch'),
+        ]);
     }
 
     public function multiUploadAction(Request $request)
